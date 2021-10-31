0c649cd024676466f10fba4120119140bf3519cd
Polina Bystrevskaya <58703112+bystrevskaya@users.noreply.github.com>
2021-10-31 22:01:24 +0700

    add application.js

---
+++ application.js
@@ -0,0 +1,28 @@
+const {Client} = require("pg")
+const client = new Client({
+    "host": "localhost",
+    "port": 5432,
+    "user": "postgres",
+    "password": "databaseadmin",
+    "database": "Library"
+})
+
+client.connect();
+const dbQuery = "select ib.issuedate, ib.returndate, s.studentname from issuedbook ib join student s on ib.studentid = s.studentid";
+client.query(dbQuery, (err, res) => {
+    if (err) throw err;
+    else getMaliciousReader(res.rows);
+    client.end;
+    }
+);
+
+function getMaliciousReader(issuedBooks) {
+    let maliciousReaders = [];
+    issuedBooks.map(issued => {
+        if (issued['returndate'] == null) issued['returndate'] = new Date();
+        let diff = new Date(issued['returndate'].getTime() - issued['issuedate'].getTime());
+        if (diff.getUTCMonth() >= 2 & !(maliciousReaders.includes(issued['studentname']))) maliciousReaders.push(issued['studentname']);
+    });
+    maliciousReaders.map(reader => console.log(reader));
+  }
+
